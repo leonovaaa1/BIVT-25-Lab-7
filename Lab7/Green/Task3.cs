63bc5dc34b62a7a611b5056d738fc0a58ea0bd66
leonovaaa1 <mariel3onova@yandex.ru>
2026-03-02 19:51:40 +0300

    Update Task3.cs

--- Lab7/Green/Task3.cs
+++ Lab7/Green/Task3.cs
@@ -16,13 +16,13 @@ namespace Lab7.Green
             {
                 _name = name;
                 _surname = surname;
-                _marks = new int[3]; 
+                _marks = new int[3];
                 _isExpelled = false;
             }
 
             public string Name => _name;
             public string Surname => _surname;
-            public int[] Marks => _marks.ToArray(); 
+            public int[] Marks => _marks.ToArray();
             public bool IsExpelled => _isExpelled;
 
             public double AverageMark
@@ -30,26 +30,38 @@ namespace Lab7.Green
                 get
                 {
                     if (_marks == null) return 0;
-                    return _marks.Where(m => m > 0).DefaultIfEmpty(0).Average();
+
+                    int sum = 0;
+                    int count = 0;
+
+                    foreach (int m in _marks)
+                    {
+                        if (m != 0)
+                        {
+                            sum += m;
+                            count++;
+                        }
+                    }
+                    if (count == 0) return 0;
+
+                    return (double)sum /count ;
                 }
             }
             public void Exam(int mark)
             {
                 if (_marks == null) return;
-
-                
                 if (_isExpelled) return;
 
-                
+
                 for (int i = 0; i < _marks.Length; i++)
                 {
                     if (_marks[i] == 0)
                     {
-                        _marks[i] = mark;  
+                        _marks[i] = mark;
 
                         if (mark == 2)
                         {
-                            _isExpelled = true;  
+                            _isExpelled = true;
                         }
                         break;
                     }
@@ -58,10 +70,24 @@ namespace Lab7.Green
             public static void SortByAverageMark(Student[] array)
             {
                 if (array == null) return;
-                Array.Sort(array, (a, b) => b.AverageMark.CompareTo(a.AverageMark));
+
+                int n = array.Length;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    for (int j = 0; j < n - i - 1; j++)
+                    {
+                       
+                        if (array[j].AverageMark < array[j + 1].AverageMark)
+                        {
+                            Student temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                        }
+                    }
+                }
             }
 
-            
+
             public void Print()
             {
                 Console.WriteLine($"Имя: {_name}, Фамилия: {_surname}, Средний балл: {AverageMark}, Отчислен: {_isExpelled}");
